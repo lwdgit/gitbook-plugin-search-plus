a394e593b44be649a5286e3b117364813472fd6e
wenli.lw <wenli.lw@alibaba-inc.com>
2017-01-14 14:16:11 +0800

    update example

--- book.js
+++ book.js
@@ -5,7 +5,7 @@ module.exports = {
     root: './content',
     output: './book',
     title: 'test search plus plugin',
-    plugins: ['-lunr', '-search','search-plus'],
+    plugins: ['-lunr', '-search','search-plus','fontsettings'],
 
     variables: {
         version: pkg.version
